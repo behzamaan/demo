18984915f2e8a597fe1295ee785e3225dc9cd968
behzamaan <behzamaan@gmail.com>
2018-03-09 01:42:05 +0330

    change

--- src/main/java/com/example/demo/TestController.java
+++ src/main/java/com/example/demo/TestController.java
@@ -8,17 +8,29 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController()
-@RequestMapping(path="/api")
+@RequestMapping(path="/api/heroes")
 public class TestController {
-    @GetMapping(path = "/hello-world")
-    public String helloWorld(){
-        return "Hello world!";
+    @GetMapping(path = "")
+    public ResponseEntity<List<Hero>> init (){
+        Hero mohamad=new Hero();
+        mohamad.setId(1L);
+        mohamad.setName("mohmad");
+        Hero hoda=new Hero();
+        hoda.setId(2L);
+        hoda.setName("hoda");
+        ArrayList<Hero> list = new ArrayList<>();
+        list.add(mohamad);
+        list.add(hoda);
+        return new ResponseEntity<List<Hero>>(list,HttpStatus.OK);
     }
-    @GetMapping(path = "/heroes/{id}")
+    @GetMapping(path = "/{id}")
     public ResponseEntity<?> getHero(@PathVariable long id){
         System.out.println(id);
-        return new ResponseEntity<>(new Hero(), HttpStatus.ACCEPTED);
+        return new ResponseEntity<>(new Hero(), HttpStatus.OK);
     }
 
 
